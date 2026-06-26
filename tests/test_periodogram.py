856d6c198e39bba29ee3b0d019d75fcc6fd3e27d
Hamy kaul <hammykaul@Hamys-Mac-mini.local>
2026-06-26 18:25:54 +0530

    Add A Jupyter Notebook to test all Modules

--- tests/test_periodogram.py
+++ tests/test_periodogram.py
@@ -6,7 +6,7 @@ from astrotime.periodogram import compute_lombscargle, find_best_period, plot_pe
 lc = load_lightcurve('lightcurve_dataset.csv')
 time =lc["time"].to_numpy()
 flux =lc["flux"].to_numpy()
-periods, power = compute_lombscargle(time,flux,min_period=1,max_period=20)
+periods, power = compute_lombscargle(time,flux,min_period=0.2,max_period=1.0)
 best_period,best_power = find_best_period(periods,power)
 print("Recovered Best Period =",best_period)
 plot_periodogram(periods, power, best_period=best_period,show=True)
